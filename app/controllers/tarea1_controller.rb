8b24f323d271227033e84ed417fc2c03bc46938c
ahinri <afhinrichsen@uc.cl>
2016-03-16 23:21:34 -0300

    validar

--- app/controllers/tarea1_controller.rb
+++ app/controllers/tarea1_controller.rb
@@ -6,21 +6,13 @@ class Tarea1Controller < ApplicationController
  	def validarFirma
  		mensaje = params[:mensaje]
  		hash = params[:hash].downcase
-
  		require 'digest'
 		digest = Digest::SHA256.hexdigest mensaje
-
-
-    	render json: {"mensaje":mensaje,"valido": (digest==hash),"hash":hash,"digest":digest}
+    	render json: {"mensaje":mensaje,"valido": (digest==hash)}
  	end
 
  	def status
-    	mensaje = "ABCDE"
- 		hash = "F0393FEBE8BAAA55E32F7BE2A7CC180BF34E52137D99E056C817A9C07B8F239A"
 
- 		require 'digest'
-
-		digest = Digest::SHA256.hexdigest mensaje
-    	render json: {"mensaje":mensaje,"digest":digest}
+    	render json: {"mensaje":"mensaje"}
  	end
 end
